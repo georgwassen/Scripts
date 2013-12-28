b36a74823c1af29681c1022d667a76d9dc413aef
Georg Wassen <georg.wassen@googlemail.com>
2013-12-28 22:19:51 +0100

    derive initialization/cloning from .git/logs

--- gitanalyzer.sh
+++ gitanalyzer.sh
@@ -22,6 +22,7 @@ COLERROR='\033[1;31m'
 COLDIR='\033[1;34m'
 COLDATE='\033[1;33m'
 COLREMOTE='\033[1;31m'
+COLACTION='\033[1;36m'
 COLRESET='\033[0m'
 
 # parse command line arguments
@@ -96,11 +97,33 @@ echo -n "init'ed or clone'd most probably on: "
 stat -c '%Y %y %n' $DIR/.git/* | sort | head -n1 | awk '{print "'$COLDATE'" $2 " " $3  "'$COLRESET' (" $5 ")" }'
 
 
+# short history (derived from .git/logs)
+#   convert UNIX time stamp to readable format: date -d @1386429090 +'%Y-%m-%d %H:%M:%S'
+
+read REV0 REV1 REST < <(head -n1 $DIR/.git/logs/HEAD)
+#echo "REV0   = '$REV0'"
+#echo "REV1   = '$REV1'"
+NAME=${REST%% <*}
+#echo "NAME   = '$NAME'"
+REST=${REST##* <}
+MAIL=${REST%%>*}
+#echo "MAIL   = '$MAIL'"
+REST=${REST##*> }
+read TIME ZONE ACTION < <(echo $REST )
+#echo "TIME   = '$TIME'"
+#echo "ZONE   = '$ZONE'"
+DATE=$(date -d@$TIME  +'%Y-%m-%d %H:%M:%S')
+#echo "ACTION = '$ACTION'"
+if [[ -z $ACTION ]]; then
+    ACTION="git init"
+fi
+
+echo -e "Source of this Repo: $COLACTION$ACTION$COLRESET on $COLDATE$DATE$COLRESET"
 
 # remote links
 echo -n "Remote links: "
 echo -en "$COLREMOTE"
-git remote -v | cut  -f2 | cut -d' ' -f1 | sort | uniq
+git remote -v | cut  -f1-2 | cut -d' ' -f1 | sort | uniq
 echo -en "$COLRESET"
 
 # git-svn
