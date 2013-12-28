b464d58712c23ef13728c882c235d7fd7991cb89
Georg Wassen <georg.wassen@googlemail.com>
2013-12-28 18:07:51 +0100

    use colors for output

--- gitanalyzer.sh
+++ gitanalyzer.sh
@@ -18,6 +18,12 @@
 #      REVISION:  ---
 #===============================================================================
 
+COLERROR='\033[1;31m'
+COLDIR='\033[1;34m'
+COLDATE='\033[1;33m'
+COLREMOTE='\033[1;31m'
+COLRESET='\033[0m'
+
 # parse command line arguments
 
 function usage() {
@@ -76,20 +82,26 @@ else
     done
 fi
 if [[ -z $DIR ]]; then
-    echo "ERROR: no .git directory found in path '$PARAM_WD'"
+    echo -e $COLERROR"ERROR: no .git directory found in path '$PARAM_WD'"$COLRESET
     exit
 fi
 
-echo "using Git base dir '$DIR'"
+echo -e "using Git base dir $COLDIR'$DIR'$COLRESET"
+
+
 
 # try to derive the age (date of init or clone) from .git files
 # (use oldest file in .git directory)
 echo -n "init'ed or clone'd most probably on: "
-stat -c '%Y %y %n' $DIR/.git/* | sort | head -n1 | awk '{print $2 " " $3  " (" $5 ")" }'
+stat -c '%Y %y %n' $DIR/.git/* | sort | head -n1 | awk '{print "'$COLDATE'" $2 " " $3  "'$COLRESET' (" $5 ")" }'
+
+
 
 # remote links
 echo -n "Remote links: "
+echo -en "$COLREMOTE"
 git remote -v | cut  -f2 | cut -d' ' -f1 | sort | uniq
+echo -en "$COLRESET"
 
 # git-svn
 echo -n "Git-SVN info: "
