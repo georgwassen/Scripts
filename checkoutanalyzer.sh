b1d1e4adee337519ed17efda54055555919ee211
Georg Wassen <georg.wassen@googlemail.com>
2014-01-05 19:15:22 +0100

    show info on Git and Svn Repos (last revision, current state, etc.)

--- checkoutanalyzer.sh
+++ checkoutanalyzer.sh
@@ -20,6 +20,13 @@
 
 DIR=~/checkout
 
+COLRED='\033[1;31m'
+#COLWHITE='\033[1;37m'
+COLBLUE='\033[1;34m'
+COLGREEN='\033[1;32m'
+COLYELLOW='\033[1;33m'
+COLRESET='\033[0m'
+
 cd $DIR
 for D in *; do
     if [[ -d $D ]]; then
@@ -28,15 +35,27 @@ for D in *; do
         if [[ -d .git ]]; then
             # Git
 
-            STR="Git"
+            STR=$(echo -e "${COLYELLOW}Git${COLRESET} ")
+            STR=$STR"$(git -c color.status=always status -sb | tr '\n' ' ' | tr -d '#' )"
         elif [[ -d .svn ]]; then
-            # Subversion 
+            # Subversion
 
+            HEAD=$(LANG=C svn info -rHEAD | sed -n 's/^Revision: \([0-9]\+\)$/\1/p')
+            V=$(svnversion 2>&1)
+            if [[ $V =~ ^svn: ]]; then
+                V=$(echo -e "${COLWHITE}Error: ${V}${COLRESET} ")
+            elif [[ $V =~ M ]]; then
+                V=$(echo -e "${COLRED}${V}${COLRESET} ")
+            elif [[ $V =~ : ]]; then
+                V=$(echo -e "${COLGREEN}${V}${COLRESET} ")
+            fi
+            V="[${HEAD}] $V"
 
-            STR="Subversion"
+            STR=$(echo -e "${COLBLUE}Svn${COLRESET} ")
+            STR="$STR$V"
         else
             # unknown
-            STR="unknown format"
+            STR=$(echo -e "${COLRED}unknown${COLRESET} ")
         fi
 
         printf "%-20s : %s\n" "$D" "$STR"
